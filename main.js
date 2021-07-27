c9c151a74827530a2cf61d4b1701122b75a40e54
Rui Frank <ruifrankpbs@gmail.com>
2021-07-26 22:31:49 -0300

    include swiper and Scroll Reveal

--- main.js
+++ main.js
@@ -30,3 +30,25 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/*====swiper=====*/
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+/*scrollReveal: Mostrar elementos quando der scroll na pagina*/
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .image, #home .text, #about .image, #about .text, #services header, #service .card, #testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links`,
+  { interval: 100 }
+)
